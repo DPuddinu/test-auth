6ad71c54220055dad3c36a8014796304f839bbd4
Dario Puddinu <dpuddinu.dev@gmail.com>
2024-05-30 10:29:18 +0200

    chore: middleware

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [auth()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    edgeMiddleware: true
+  })
 });
